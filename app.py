2443b32b907388f70f5561300f9512a06019e3aa
kelvinkipsang <kelvinkipsang@gmail.com>
2016-09-07 17:30:05 +0300

    importing modules and decorators

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,url_for,redirect
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,6 +9,16 @@ def index():
 @app.route('/welcome')
 def welcome():
     return render_template("welcome.html")
+
+@app.route('/login',methods = ['GET','POST'])
+def login():
+    error = None
+    if request.method=='POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin' :
+            error = 'Invalid credentials. Please enter correct details'
+        else:
+            return (redirect(url_for('home')))
+    return render_template('login.html',error=error)
 
 if __name__== '__main__':
     app.run(debug=True)
