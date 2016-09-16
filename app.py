77c2dba4ec1cdb6519957c52347c55643e4482a9
kelvinkipsang <kelvinkipsang@gmail.com>
2016-09-16 15:29:12 +0300

    correcting login system

--- app.py
+++ app.py
@@ -3,14 +3,27 @@ from functools import wraps
 
 app = Flask(__name__)
 
-app.secret_key = "secret key"  #encryption key to access session data on server side
+app.secret_key = "im batman"  #encryption key to access session data on server side
+
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Log in first')
+            return redirect(url_for('login'))
+    return wrap
 
 
 @app.route('/')
+@login_required
 def home():
     return render_template("index.html")
 
 @app.route('/welcome')
+
 def welcome():
     return render_template("welcome.html")
 
@@ -20,6 +33,8 @@ def login():
     if request.method =='POST':
         if request.form['username'] != 'admin' or request.form['password'] != 'admin' :
             error = 'Invalid credentials. Please enter correct details'
+            return render_template('login.html', error=error)
+
         else:
             session['logged in'] = True
             flash("you logged in")
@@ -27,10 +42,11 @@ def login():
     return render_template('login.html',error=error)
 
 @app.route('/logout')
+@login_required
 def logout():
     session.pop('logged in', None) #pop out true replacing it with None
     flash("you logged out")
-    return redirect(url_for('login')) #redirect to login page
+    return redirect(url_for('welcome')) #redirect to login page
 
 
 if __name__ == '__main__':
--- sql.py
+++ sql.py
@@ -3,5 +3,5 @@ import sqlite3
 #creating connection to db/create db if it doesnt exist
 with sqlite3.connect("sample.db") as connection:
     c = connection.cursor()                                             #define cursor (for db interaction) & assign var c to it
-    c.execute("""CREATE TABLE posts(title TEXT, description TEXT)""")  #create new table(posts) 2 columns title &text
-    c.execute('INSERT INTO posts VALUES ("kelvin","a quiet guy")')                                      #add data to posts
+    c.execute("CREATE TABLE posts(title TEXT, description TEXT)")   #create new table(posts) 2 columns title &text
+    c.execute('INSERT INTO posts VALUES ("kelvin","a quiet guy")')      #add data to posts
